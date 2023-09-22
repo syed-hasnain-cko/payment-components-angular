67878bc5d4ab9058285c1d52f43faf43c5b06f4f
syed.hasnain <syed.hasnain@checkout.com>
2023-09-23 01:24:13 +0200

    add secret key in env file

--- client/src/app/services/checkout.service.ts
+++ client/src/app/services/checkout.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders  } from '@angular/common/http';
+import { environment } from 'src/environments/environment.sandbox';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-  authorization = 'Bearer sk_sbox_dqmcmja373yetcnwkrwi6x6biyv';
+ 
+  authorization = environment.checkoutSecretKey;
+
   baseURL = 'https://api.sandbox.checkout.com'
  
   constructor(private http:HttpClient) { 
-    
-  }
+
+   }
 
   requestPaymentSession(body:any):Observable<any>{
 
---
+++ client/src/environments/environment.sandbox.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    checkoutSecretKey: 'Bearer sk_sbox_dqmcmja373yetcnwkrwi6x6biyv',
+  };
